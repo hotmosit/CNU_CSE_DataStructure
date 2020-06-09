a85585bbb54450049274fb970760e5947a334814
hotmosit <gye0203@gmail.com>
2020-06-09 14:27:56 +0900

    DS assignment for week 11 - tree

--- DS08_11/Problem_3.java
+++ DS08_11/Problem_3.java
@@ -2,7 +2,7 @@ package DS08_11;
 
 
 public class Problem_3 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         BinaryTree bt1 = new BinaryTree();
         BinaryTree bt2 = new BinaryTree();
 
@@ -14,7 +14,7 @@ public class Problem_3 {
         System.out.println("<< 후위 순회 시작 >>");
         bt1.postorder(bt1);
         System.out.println();
-        System.out.println("트리의 높이 : "+ bt1.height(bt1));
+        System.out.println("트리의 높이 : " + bt1.height(bt1));
 
         System.out.println();
 
@@ -57,25 +57,25 @@ class BinaryTree {
 
     public void insert(int x) {
 
-        if(key == -1){
+        if (key == -1) {
             // 디폴트 생성일 시
             key = x;
             return;
         }
 
         if (x < key) {
-            if(this.left == null) {
+            if (this.left == null) {
                 // 왼쪽에 노드가 없는 경우 노드를 생성하여 key 값을 넣는다
                 this.left = new BinaryTree(x);
-            }else {
+            } else {
                 // 왼쪽에 노드가 있는 경우 insert(x) 실행
                 this.left.insert(x);
             }
         } else {
-            if(this.right == null){
+            if (this.right == null) {
                 // 오른쪽에 노드가 없는 경우 노드 생성하여 key 값 넣는다.
                 this.right = new BinaryTree(x);
-            }else {
+            } else {
                 // 오른쪽 노드가 있는 경우 insert(x) 실행
                 this.right.insert(x);
             }
@@ -86,18 +86,18 @@ class BinaryTree {
     public void postorder(BinaryTree bt) {
         // 재귀 호출 이용하여 구현
 
-        if( bt.right == null && bt.left == null){
+        if (bt.right == null && bt.left == null) {
             // 양쪽에 노드가 비어있는 경우 재귀 탈출 (base)
-            System.out.print(bt.key+ " ");
+            System.out.print(bt.key + " ");
             return;
         }
 
 
-        if(bt.left != null) {
+        if (bt.left != null) {
             // 왼쪽에 노드가 있는 경우 왼쪽을 먼저 탐색한다.
             postorder(bt.left);
         }
-        if(bt.right!= null) {
+        if (bt.right != null) {
             // 왼쪽 탐색을 마친 후 오른쪽을 탐색한다.
             postorder(bt.right);
         }
@@ -106,34 +106,30 @@ class BinaryTree {
     }
 
     public int height(BinaryTree bt) {
-        if(bt == null){
-            // 노드가 null인 경우 높이가 0이므로 0반환
-            return 0;
+        int left_height, right_height;
+
+        if (bt.left != null) {
+            // 왼쪽에 노드가 존재하는 경우 leftS에 재귀 호출
+            left_height = height(bt.left);
         } else {
-            int left_height, right_height;
-
-            if(bt.left != null) {
-                // 왼쪽에 노드가 존재하는 경우 leftS에 재귀 호출
-                left_height = height(bt.left);
-            }  else {
-                // 존재하지 않는 경우 -1로 설정한다.
-                left_height = -1;
-            }
-            if(bt.right != null){
-                // 오른쪽에 노드가 존재하는 경우 rightS에 재귀 호출
-                right_height = height(bt.right);
-            }else {
-                // 존재하지 않는 경우 -1로 설정한다.
-                right_height = -1;
-            }
-
-            if(left_height > right_height){
-                // 왼쪽이 오른쪽보다 큰 경우
-                return 1 + left_height;
-            } else {
-                // 오른쪽이 왼쪽보다 큰 경우
-                return 1 + right_height;
-            }
+            // 존재하지 않는 경우 -1로 설정한다.
+            left_height = -1;
         }
+        if (bt.right != null) {
+            // 오른쪽에 노드가 존재하는 경우 rightS에 재귀 호출
+            right_height = height(bt.right);
+        } else {
+            // 존재하지 않는 경우 -1로 설정한다.
+            right_height = -1;
+        }
+
+        if (left_height > right_height) {
+            // 왼쪽이 오른쪽보다 큰 경우
+            return 1 + left_height;
+        } else {
+            // 오른쪽이 왼쪽보다 큰 경우
+            return 1 + right_height;
+        }
+
     }
 }
